d26840b635ac5add6c92c001421334e1d8b2d1d1
macfluffy <shadowimitation@hotmail.com>
2026-03-21 12:02:56 +1100

    feat: Renamed the folder so we can work on AWS connection separately

---
+++ terraform_local/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">=1.0.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.7.0"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "hello" {
+  filename = var.file_name
+  content  = var.file_content
+}
+
+resource "local_file" "hello1" {
+  filename = var.file_name
+  content  = var.file_content
+}
+
+resource "local_file" "hello2" {
+  filename = var.file_name
+  content  = var.file_content
+}
